7afcdca7d76bfa6945ad28001d2aafcd8479e624
Paul Hopkins <pro1polaris@me.com>
2017-04-27 22:41:21 -0700

    Prepare Xcode Project
    Created new Xcode Project for Extension Files

---
+++ SwiftEssentials/SwiftEssentials/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftEssentials
+//
+//  Created by Paul Hopkins on 2017-04-27.
+//  Copyright © 2017 Paul Hopkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+
+}
+
